03cec6eabdaefe61e7f35af8ba169ba765de21dd
KhaledAlabssi <khaled.absi5@gmail.com>
2024-03-31 19:39:31 +0200

    add env and connect the db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,26 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
+    mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Mongo is connected successfully"))
+    .catch(err => console.error(err))
+
 
 app.listen(PORT, () => {
     console.log(`Server runing on ${PORT}`);
 })
+
+app.use(cors({
+    origin: ["http://localhost:5000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
+
+app.use(express.json())
