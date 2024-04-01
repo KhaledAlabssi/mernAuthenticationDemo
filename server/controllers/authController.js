9e1dd65bb949f953b44def5a9ab85d216e1a7f4a
KhaledAlabssi <khaled.absi5@gmail.com>
2024-04-01 11:15:21 +0200

    add login functionality and finalize auth routes in server dir

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,4 +24,30 @@ const Signup = async (req, res, next) => {
     }
 }
 
-export {Signup}
+const Login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.json({message: 'All Fields are required'})
+        }
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.json({message: "Incorrect password or email"})
+        }
+        const auth = await bcrypt.compare(password, user.password)
+        if (!auth) {
+            return res.json({message: "Incorrect password or email"})
+        }
+        const token = createSecretToken(user._id)
+        res.cookie("token", token, {
+            withCredentials: true,
+            httpOnly: false
+        })
+        res.status(201).json({ message: "User Logged in successfully", success: true })
+        next()
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export {Signup, Login}
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,7 +1,11 @@
-import { Signup } from '../controllers/authController.js'
+import { Signup, Login } from '../controllers/authController.js'
 import express from 'express'
+import { userVerification } from '../middlewares/authMiddleware.js'
 const router = express.Router()
+router.route('/').post(userVerification)
 router.route('/signup').post(Signup)
+router.route("/login").post(Login)
+
 
 
 export default router
